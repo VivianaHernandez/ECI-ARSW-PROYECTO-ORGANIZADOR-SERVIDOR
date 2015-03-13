d4fd3b72f2571324e4d4bd444c0743f0e00b9cff
VivianaHernandez13 <viviana.hernandez@mail.escuelaing.edu.co>
2015-03-12 21:41:52 -0500

    crear

---
+++ src/main/java/edu/eci/arsw/lab/Server/ScreenCaptureStub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.lab.Server;
+
+import java.awt.image.BufferedImage;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author viviana
+ */
+public interface ScreenCaptureStub 
+{
+  public String getScreenShot() throws ScreenCaptureException;
+ 
+ 
+    
+}
